5026a6ab4e7eb275b6f9cd3498389b64e4a95af3
OnlyTwentyCharacters <qxest@reqxest.com>
2017-05-18 23:09:26 +0000

    Fix up with semistandard and any crap code

--- models/user.js
+++ models/user.js
@@ -5,20 +5,20 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 // set up a mongoose model and pass it using module.exports
 const userSchema = new Schema({
-	email: String,
-	admin: Boolean,
-	verified: Boolean,
-	resetPasswordToken: String,
-	resetPasswordExpires: Date
+  email: String,
+  admin: Boolean,
+  verified: Boolean,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 userSchema.plugin(passportLocalMongoose, ({usernameLowerCase: true, usernameQueryFields: ['email'], limitAttempts: true, maxAttempts: 20}));
 
 // override toJSON so that the password hash isn't returned
-userSchema.methods.toJSON = function() {
-	var obj = this.toObject();
-	delete obj.password;
-	return obj;
+userSchema.methods.toJSON = function () {
+  var obj = this.toObject();
+  delete obj.password;
+  return obj;
 };
 
 // create the model for users and expose to the app
--- views/assets/js/init.js
+++ views/assets/js/init.js
@@ -1,57 +1,57 @@
-/*eslint no-undef: "error"*/
-/*eslint-env browser*/
+const jQuery = window.jQuery;
+const fetch = window.fetch;
+const Request = window.Request;
 
-(function($) {
-	$(function() {
+(function ($) {
+  $(function () {
+    $(function () {
+      $('#login-form').submit(function (e) {
+        var request = new Request('/login', {
+          method: 'POST',
+          redirect: 'follow',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: $('#login-form').serialize(),
+          cors: 'same-origin',
+          credentials: 'include'
+        });
 
-		$(function() {
-			$('#login-form').submit(function(e) {
-				var request = new Request('/login', {
-					method: 'POST',
-					redirect: 'follow',
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded',
-					},
-					body: $('#login-form').serialize(),
-					cors: 'same-origin',
-					credentials: 'include'
-				});
+        fetch(request).then(response => response.json()).then(response => {
+          if (response.error) {
+            $('#error-container span').text(response.message);
+            $('#error-container').removeClass('hidden');
+          } else {
+            window.location.href = '/notes';
+          }
+        }).catch(console.error);
 
-				fetch(request).then(response => response.json()).then(response => {
-					if (response.error) {
-						$('#error-container span').text(response.message);
-						$('#error-container').removeClass('hidden');
-					} else {
-						window.location.href = '/notes';
-					}
-				}).catch(console.error);
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+      });
 
-				e.preventDefault(); // avoid to execute the actual submit of the form.
-			});
-			
-			$('#register-form').submit(function(e) {
-				var request = new Request('/register', {
-					method: 'POST',
-					redirect: 'follow',
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded',
-					},
-					body: $('#register-form').serialize(),
-					cors: 'same-origin',
-					credentials: 'include'
-				});
+      $('#register-form').submit(function (e) {
+        var request = new Request('/register', {
+          method: 'POST',
+          redirect: 'follow',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: $('#register-form').serialize(),
+          cors: 'same-origin',
+          credentials: 'include'
+        });
 
-				fetch(request).then(response => response.json()).then(response => {
-					if (response.error) {
-						$('#error-container span').text(response.message);
-						$('#error-container').removeClass('hidden');
-					} else {
-						window.location.href = '/login';
-					}
-				}).catch(console.error);
+        fetch(request).then(response => response.json()).then(response => {
+          if (response.error) {
+            $('#error-container span').text(response.message);
+            $('#error-container').removeClass('hidden');
+          } else {
+            window.location.href = '/login';
+          }
+        }).catch(console.error);
 
-				e.preventDefault(); // avoid to execute the actual submit of the form.
-			});
-		});
-	}); // end of document ready
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+      });
+    });
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
